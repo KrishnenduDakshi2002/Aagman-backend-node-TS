53bfe55a8e4ad6fa476a0424bf08ff59db2c3530
krishnendu Dakshi <kdakshi2020@gamil.com>
2022-11-08 06:55:38 +0530

    hostname chnaged

--- config/default.ts
+++ config/default.ts
@@ -2,6 +2,7 @@
 require('dotenv/config')
 
 export default {
+    HOSTNAME : '192.168.29.167',
     PORT : 8000 || process.env.PORT,
     DB_URI : process.env.DB_CONNECTION,
     TOKEN_SECRET : "dfasdjrqwu3409234dfadadfowerq3432frwij23490oi",
