341dfa218704d1f8e400609de3b3f42b277d1f09
krishnendu Dakshi <kdakshi2020@gamil.com>
2022-11-22 15:58:45 +0530

    added dist folder for netlify

--- config/default.ts
+++ config/default.ts
@@ -2,7 +2,7 @@
 require('dotenv/config')
 
 export default {
-    HOSTNAME :  '192.168.240.101',
+    HOSTNAME :  '192.168.36.101',
     PORT : 8000 || process.env.PORT,
     DB_URI : process.env.DB_CONNECTION,
     TOKEN_SECRET : "dfasdjrqwu3409234dfadadfowerq3432frwij23490oi",
